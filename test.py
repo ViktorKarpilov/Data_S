38c4a4c5c04c3c36aa02578ee7a42cf8e2d697b4
ViktorKarpilov <wktorion@gmail.com>
2018-12-01 14:58:03 +0200

    Update name

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ xnew = np.arange(0, 9, 0.1)
 ynew = f(xnew)
 znew = f(ynew)
 # use interpolation function returned by `interp1d`
-plotly.offline.plot([plotly.graph_objs.Scatter(x=x,y=y),plotly.graph_objs.Scatter3d(x=xnew,y=ynew,z=znew)],auto_open=True)
+plotly.offline.plot([plotly.graph_objs.Scatter(x=x,y=y),plotly.graph_objs.Scatter3d(x=xnew,y=ynew,z=znew)],auto_open=True,filename="interpolation.html")
 
 
 
